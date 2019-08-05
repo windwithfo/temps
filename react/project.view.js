e488f695a7d000411730c837bd7527ada55c87db
dongkunshan <windwithfo@yeah.net>
2019-08-05 13:08:51 +0800

    update react temp

---
+++ react/project.view.js
@@ -0,0 +1,43 @@
+/**
+ * @file 入口页面配置
+ * @author dongkunshan(windwithfo@yeah.net)
+ */
+
+module.exports = {
+  index: {
+    template: 'html.ejs',
+    path: 'view/index/index.jsx',
+    title: 'index',
+    meta: {
+      keywords: 'index',
+      description: 'index',
+      viewport: 'initial-scale=1, maximum-scale=1',
+      'format-detection': 'telephone=no',
+      'format-detection': 'email=no'
+    }
+  },
+  page1: {
+    template: 'html.ejs',
+    path: 'view/page1/page1.jsx',
+    title: 'page1',
+    meta: {
+      keywords: 'page1',
+      description: 'page1',
+      viewport: 'initial-scale=1, maximum-scale=1',
+      'format-detection': 'telephone=no',
+      'format-detection': 'email=no'
+    }
+  },
+  page2: {
+    template: 'html.ejs',
+    path: 'view/page2/page2.jsx',
+    title: 'page2',
+    meta: {
+      keywords: 'page2',
+      description: 'page2',
+      viewport: 'initial-scale=1, maximum-scale=1',
+      'format-detection': 'telephone=no',
+      'format-detection': 'email=no'
+    }
+  }
+}
